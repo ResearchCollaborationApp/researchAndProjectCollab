9f28dac155cbd2141420f44168a304b58131a26c
Bhuwan Bhandari <bhuwanbhandari2821@gmail.com>
2024-07-02 09:05:30 -0400

    App.js modified in bhuwan

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import UserInformation from './Components/userinformationforms/userInformation.js'
+import ResearchForm from './Components/userinformationforms/researchForm.js'
 function App() {
   return (
     <>
-      <UserInformation/>
+      <ResearchForm/>
     </>
   )
 
