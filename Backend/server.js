932e0e0ae78aef4fa02868169d021a16bde2b1cb
Bhuwan Bhandari <bhuwanbhandari2821@gmail.com>
2024-08-01 19:53:03 -0400

    skills handled

--- Backend/server.js
+++ Backend/server.js
@@ -32,6 +32,7 @@ const getCollection = (dbName, collectionName) => {
   return db.collection(collectionName); // Access the collection
 };
 
+
 app.use(cors({
   origin: "http://localhost:3000",
   methods: "GET, POST, PUT, DELETE",
@@ -44,14 +45,19 @@ app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
+app.get("/getResearch/api/skills", async(req, res)=>{
+  const collection = getCollection("skills", "skills");
+  const skills = await collection.find({}).toArray();
+  res.json(skills);
+})
 
 app.get("/api/posts", async (req, res)=>{
   const offset = parseInt(req.query.offset) || 0;
   const limit = parseInt(req.query.limit) || 10;
   const collection = getCollection("posts", "posts")
   try {
-    const skills = await collection.find().skip(offset).limit(limit).toArray();
-    res.json(skills);
+    const posts = await collection.find().skip(offset).limit(limit).toArray();
+    res.json(posts);
   } catch (error) {
     res.status(500).json({ message: 'Error fetching movies', error });
   }
