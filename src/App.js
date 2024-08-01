15fa2ffb2d089a9651f491df962c3c2bf69bade4
Bhuwan Bhandari <bhuwanbhandari2821@gmail.com>
2024-08-01 19:53:37 -0400

    skills page api handled

--- src/App.js
+++ src/App.js
@@ -31,8 +31,8 @@ const router = createBrowserRouter(
 function App() {
   return (
     <>
-      {/* <RouterProvider router = {router}/> */}
-      <Mainprofile/>
+      <RouterProvider router = {router}/>
+      
     </>
   )
 
