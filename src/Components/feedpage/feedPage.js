1c361238275532706a4c173febef8d1e703329c6
Bhuwan Bhandari <bhuwanbhandari2821@gmail.com>
2024-08-07 10:52:26 -0400

    add post added

--- src/Components/feedpage/feedPage.js
+++ src/Components/feedpage/feedPage.js
@@ -3,6 +3,8 @@ import UserInfo from "../profilepage/userInfo"
 import AutoScroll from "./autoScroll"
 import ScrollingFeed from "./scrollingfeed/scrollingFeed"
 import TopResearch from "./topResearch"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from "@fortawesome/free-solid-svg-icons"
 export default function FeedPage() {
   return (
     <>
@@ -18,18 +20,18 @@ export default function FeedPage() {
         </div>
       </div>
       <div className="h-fit col-span-1 lg:col-span-3">
-        <div className = "h-20 w-full bg-white mb-4 border rounded">
-          <div className="flex items-center py-1 px-3">
+        <div className = "flex-col border rounded-md items-center h-fit w-full bg-white mb-4 border">
+          <div className="flex  items-center gap-4 py-1 px-3">
             <img
             alt=""
             src="https://images.unsplash.com/photo-1614644147724-2d4785d69962?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
             className="w-10 h-10 items-center rounded-full object-cover"
           />
-            <div className = "items-start ml-3 p-3 mt-3  bg-gray-200 rounded border border-slate-300">
+            <div className = "items-start font-medium ml-3 my-3 w-2/3 h-fit p-3 bg-gray-200 border rounded-3xl border-slate-300">
               Share something...
             </div>
+            <FontAwesomeIcon icon={faPlus} className="size-6 "/>
           </div>
-          
         </div>
         <ScrollingFeed/>
       </div>
