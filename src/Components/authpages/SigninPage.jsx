242e9f544cbb9f2482dd0d88124fa3d47e6cd41b
acbikash13 <acbikash13@gmail.com>
2024-09-14 16:11:55 -0400

    Added signinpage

--- src/Components/authpages/SigninPage.jsx
+++ src/Components/authpages/SigninPage.jsx
@@ -9,9 +9,6 @@ export default function SigninPage(){
     const signInWithGoogle = ()=>{
         window.open("http://localhost:4000/auth/google", "_self")
     }
-    const signInWithLinkedin = () =>{
-        window.open("http://localhost:4000/auth/linkedin", "_self")
-    }
     const signInWithMicrosoft = () => {
         window.open("http://localhost:4000/auth/microsoft", "_self");
 
@@ -47,16 +44,6 @@ export default function SigninPage(){
                             <FontAwesomeIcon icon={faGoogle} className="w-full h-full text-red-500" />
                         </div>
                         Sign in with Google
-                    </button>
-                    <button
-                        onClick = {signInWithLinkedin}
-                        type="button"
-                        className="relative inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-gray-700 transition-all duration-200 bg-white border-2 border-gray-200 rounded-md hover:bg-gray-100 focus:bg-gray-100 hover:text-black focus:text-black focus:outline-none"
-                    >
-                        <div className="absolute inset-y-0 left-0 p-4">
-                            <FontAwesomeIcon icon={faLinkedin} className="w-full h-full text-blue-600" />
-                        </div>
-                        Sign in with Linkedin
                     </button>
                 </div>
                 </div>
