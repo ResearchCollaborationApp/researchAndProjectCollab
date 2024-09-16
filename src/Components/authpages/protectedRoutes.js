0afa41f780a93a2d983efe768629301f394cdddb
Bhuwan Bhandari <bhuwanbhandari2821@gmail.com>
2024-09-16 13:43:28 -0400

    worked on protected routes

--- src/Components/authpages/protectedRoutes.js
+++ src/Components/authpages/protectedRoutes.js
@@ -2,19 +2,21 @@ import { Navigate, Outlet, useOutletContext } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 function ProtectedRoutes() {
-  {console.log("you visited private routes")}
+  {
+    console.log("you visited private routes");
+  }
   const context = useOutletContext();
-  const user = context.user;
-  const [loading, setLoading] = useState(true); // Local loading state
+  const user = context.user; 
+  const [loading, setLoading] = useState(true); //Local loading state
   const checkAuthorization = async () => {
     try {
-        const response = await fetch("/auth/check-session", {
+      const response = await fetch("/auth/check-session", {
         method: "GET",
         credentials: "include", // Include cookies in the request
       });
       const data = await response.json();
       if (data.loggedIn) {
-        console.log("user is logged in ")
+        console.log("user is logged in ");
         context.setUser(data.user);
       }
     } catch (error) {
@@ -32,7 +34,7 @@ function ProtectedRoutes() {
     return <div>Loading...</div>; // Display a loading state while checking authorization
   }
   return user ? (
-    <Outlet context = {context} />
+    <Outlet context={context} />
   ) : (
     <Navigate to="signin" replace /> // Redirect to sign-in page if not logged in
   );
